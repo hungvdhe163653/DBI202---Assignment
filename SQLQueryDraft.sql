c4f5709b2b8b5df36d426dfd3b03230c515f8d92
hungvdhe163653 <107698870+hungvdhe163653@users.noreply.github.com>
2022-07-14 10:02:43 +0700

    Update SQLQueryDraft.sql

--- SQLQueryDraft.sql
+++ SQLQueryDraft.sql
@@ -3,3 +3,7 @@
 SELECT s.StudentName FROM Student s
 ORDER BY s.StudentName ASC
 --F2
+SELECT s.StudentID, s.StudentName, g.GroupID, g.SemesterID
+FROM Student s
+INNER JOIN Groups g ON s.StudentID = g.StudentID;
+
